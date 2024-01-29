339aadefb4f1241448d092f85736e172e10d266d
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-28 23:55:21 -0300

    recolored

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,121 +1,122 @@
-import React from 'react'
+import { FaDribbble, FaFacebook, FaGithub, FaTwitter } from 'react-icons/fa'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className="relative bg-blueGray-200 pt-8 pb-6">
+    <footer className="relative bg-sky-100 pt-8 pb-6">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-3xl fonat-semibold text-blueGray-700">
+            <h4 className="text-3xl fonat-semibold text-neutral-800">
               Let&apos;s keep in touch!
             </h4>
-            <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+            <h5 className="text-lg mt-0 mb-2 text-neutral-800">
               Find us on any of these platforms, we respond 1-2 business days.
             </h5>
             <div className="mt-6 lg:mb-0 mb-6">
               <button
-                className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-blue-400 shadow-lg h-10 w-10 items-center rounded-full mr-2"
                 type="button"
               >
-                <i className="fab fa-twitter"></i>
+                <FaTwitter className="mx-auto text-lg" />
               </button>
               <button
-                className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-blue-600 shadow-lg h-10 w-10 items-center rounded-full mr-2"
                 type="button"
               >
-                <i className="fab fa-facebook-square"></i>
+                <FaFacebook className="mx-auto text-lg" />
               </button>
               <button
-                className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-pink-400 shadow-lg h-10 w-10 items-center rounded-full mr-2"
                 type="button"
               >
-                <i className="fab fa-dribbble"></i>
+                <FaDribbble className="mx-auto text-lg" />
               </button>
               <button
-                className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className="bg-white text-blue-800 shadow-lg h-10 w-10 items-center rounded-full mr-2"
                 type="button"
               >
-                <i className="fab fa-github"></i>
+                <FaGithub className="mx-auto text-lg" />
               </button>
             </div>
           </div>
           <div className="w-full lg:w-6/12 px-4">
             <div className="flex flex-wrap items-top mb-6">
               <div className="w-full lg:w-4/12 px-4 ml-auto">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                <span className="block uppercase text-blue-800 text-sm font-semibold mb-2">
                   Useful Links
                 </span>
                 <ul className="list-unstyled">
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://www.creative-tim.com/presentation?ref=njs-profile"
                     >
                       About Us
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://blog.creative-tim.com?ref=njs-profile"
                     >
                       Blog
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://www.github.com/creativetimofficial?ref=njs-profile"
                     >
                       Github
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile"
                     >
                       Free Products
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </div>
               <div className="w-full lg:w-4/12 px-4">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                <span className="block uppercase text-blue-500 text-sm font-semibold mb-2">
                   Other Resources
                 </span>
                 <ul className="list-unstyled">
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
                     >
                       MIT License
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://creative-tim.com/terms?ref=njs-profile"
                     >
                       Terms &amp; Conditions
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://creative-tim.com/privacy?ref=njs-profile"
                     >
                       Privacy Policy
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                    <Link
+                      className="text-neutral-600 hover:text-neutral-800 font-semibold block pb-2 text-sm"
                       href="https://creative-tim.com/contact-us?ref=njs-profile"
                     >
                       Contact Us
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </div>
@@ -125,22 +126,15 @@ const Footer = () => {
         <hr className="my-6 border-blueGray-300" />
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
-              Copyright © <span id="get-current-year">2021</span>
-              <a
-                href="https://www.creative-tim.com/product/notus-js"
-                className="text-blueGray-500 hover:text-gray-800"
-                target="_blank"
-              >
-                {' '}
-                Notus JS by
-              </a>
-              <a
+            <div className="text-sm text-gray-500 font-semibold py-1">
+              Copyright © <span id="get-current-year">2024</span>
+              <label className="text-gray-500 hover:text-gray-800"> by </label>
+              <Link
                 href="https://www.creative-tim.com?ref=njs-profile"
-                className="text-blueGray-500 hover:text-blueGray-800"
+                className="text-gray-500 hover:text-gray-800"
               >
-                Creative Tim
-              </a>
+                Zoma
+              </Link>
               .
             </div>
           </div>
